e405bbc7d3a187e00c980e874d7e8ad84f5927d9
Sara <sarapanoffminerva@yahoo.com>
2018-10-27 21:24:49 -0400

    Unit 5

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,17 @@
 
+const quizContainer = document.getElementById("quiz");
+const resultsContainer = document.getElementById('results');
+
+function buildQuiz(){
+}
+
+function
+    showResults(){}
+
+buildQuiz();
+
+
+const myQuestions =[
+   "In which country did Halloween originate?", "Which Catholic Church holiday is Halloween linked to?",
+   console.log("myQuestions")
+]
